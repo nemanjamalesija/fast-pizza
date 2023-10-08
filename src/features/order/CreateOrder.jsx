7ac4e7b941b49488ab9b0298d3f770733402e2f9
nemanjamalesija <malesija13@gmail.com>
2023-10-08 23:05:15 +0200

    Complete project

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -7,6 +7,7 @@ import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
 import { formatCurrency } from '../../utils/helpers';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -41,9 +42,18 @@ const fakeCart = [
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
 
+  const dispatch = useDispatch();
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: adressStatus,
+    position,
+    adress,
+    error: errorAdress,
+  } = useSelector((state) => state.user);
+
+  const isLoadingAdress = adressStatus == 'loading';
   const cart = useSelector(getCart);
   const formErrors = useActionData();
   const totalCartPrice = useSelector(getTotalCartPrice);
@@ -81,16 +91,38 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+        <div className='relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
           <label className='sm:basis-40'>Address</label>
           <div className='grow'>
             <input
               className='input w-full'
               type='text'
               name='address'
+              disabled={isLoadingAdress}
+              defaultValue={adress}
               required
             />
+            {adressStatus == 'error' && (
+              <p className='mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700'>
+                {errorAdress}
+              </p>
+            )}
           </div>
+
+          {!position.latitude && !position.longitude && (
+            <div className='absolute right-[3px] top-[35px] sm:right-[5px] sm:top-[5px] z-50'>
+              <Button
+                type='small'
+                disabled={isSubmitting || isLoadingAdress}
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get position
+              </Button>
+            </div>
+          )}
         </div>
 
         <div className='mb-12 flex items-center gap-5'>
@@ -109,7 +141,16 @@ function CreateOrder() {
 
         <div>
           <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type='primary'>
+          <input
+            type='hidden'
+            name='position'
+            value={
+              position.longitude
+                ? `${position.latitude}, ${position.longitude}`
+                : ''
+            }
+          />
+          <Button disabled={isSubmitting || isLoadingAdress} type='primary'>
             {isSubmitting
               ? 'Placing order....'
               : `Order now for ${formatCurrency(totalPrice)}`}
