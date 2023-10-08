0e8483e63a06ce2fb0aecd05bc718e264d90a7b7
nemanjamalesija <malesija13@gmail.com>
2023-10-08 22:25:20 +0200

    Complete fetch adress async thunk

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { getAddress } from '../../services/apiGeocoding';
+import { act } from 'react-dom/test-utils';
 
 function getPosition() {
   return new Promise(function (resolve, reject) {
@@ -10,25 +11,29 @@ function getPosition() {
 export const fetchAddress = createAsyncThunk(
   'user/fetchAddress',
   async function () {
-    // 1) We get the user's geolocation position
+    // 1 We get the user's geolocation position
     const positionObj = await getPosition();
     const position = {
       latitude: positionObj.coords.latitude,
       longitude: positionObj.coords.longitude,
     };
 
-    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    // 2 Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
     const addressObj = await getAddress(position);
     const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-    // 3) Then we return an object with the data that we are interested in.
+    // 3 Then we return an object with the data that we are interested in.
     // Payload of the FULFILLED state
     return { position, address };
   }
 );
 
 const initialState = {
-  username: 'Shon',
+  username: '',
+  status: 'idle',
+  position: {},
+  adress: '',
+  error: '',
 };
 
 const userSlice = createSlice({
@@ -39,6 +44,20 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.adress = action.payload.address;
+        state.status = 'idle';
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = 'error';
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateName } = userSlice.actions;
